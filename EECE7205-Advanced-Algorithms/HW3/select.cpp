e427545b31e22fd63ae0d13a83ff8a2e88107424
Indrajeet Aditya Roy <roy.indrajeet009@gmail.com>
2024-10-26 12:30:44 -0400

    implemented select

--- EECE7205-Advanced-Algorithms/HW3/select.cpp
+++ EECE7205-Advanced-Algorithms/HW3/select.cpp
@@ -7,95 +7,89 @@
 
 using namespace std;
 
-int partition(vector<int> &arr, int low, int high) {
-    int pivot = arr[high];
-    int i = low - 1;
-
-    for (int j = low; j < high; j++) {
-        if (arr[j] <= pivot) {
-            i++;
-            swap(arr[i], arr[j]);
+int partition(vector<int>& arr, int left, int right, int pivot) {
+    int pivot_idx = left;
+    for (int i = left; i <= right; i++) {
+        if (arr[i] == pivot) {
+            pivot_idx = i;
+            break;
         }
     }
-    swap(arr[i + 1], arr[high]);
-    return i + 1;
+    swap(arr[pivot_idx], arr[right]);
+    int partition_idx = left;
+    for (int i = left; i < right; i++) {
+        if (arr[i] < pivot) {
+            swap(arr[i], arr[partition_idx]);
+            partition_idx++;
+        }
+    }
+    swap(arr[partition_idx], arr[right]);
+    return partition_idx;
 }
 
-int median(vector<int> &A, int low, int high) {
-    sort(A.begin() + low, A.begin() + high + 1);
-    int mid = (low + high) / 2;
+int median(vector<int>& A, int left, int right) {
+    sort(A.begin() + left, A.begin() + right + 1);
+    int mid = (left + right) / 2;
     return A[mid];
 }
 
-int medianOfMedians(vector<int> &A, int low, int high) {
-    int n = high - low + 1;
-    
-    // Divide the array into groups of 5
-    vector<int> medians;
-    for (int i = 0; i < n / 5; i++) {
-        int groupLow = low + i * 5;
-        int groupHigh = min(groupLow + 4, high);
-        medians.push_back(median(A, groupLow, groupHigh));
+int median_of_medians(vector<int>& arr, int p, int q) {
+    int n = q - p + 1;
+    if (n <= 5) {
+        return median(arr, p, q);
     }
 
-    // If the array size is not a multiple of 5, handle the remainder
-    if (n % 5 != 0) {
-        int groupLow = low + (n / 5) * 5;
-        int groupHigh = high;
-        medians.push_back(median(A, groupLow, groupHigh));
+    int groups = (n + 4) / 5;
+    vector<int> medians(groups);
+
+    for (int i = 0; i < groups; i++) {
+        int l = p + i * 5;
+        int r = min(l + 4, q);
+        medians[i] = median(arr, l, r);
     }
 
-    // Recursively find the median of medians
-    if (medians.size() <= 5) {
-        return median(medians, 0, medians.size() - 1);
-    } else {
-        return medianOfMedians(medians, 0, medians.size() - 1);
-    }
+    return median_of_medians(medians, 0, groups - 1);
 }
 
-// Select function (worst-case linear time)
-int select(vector<int> &A, int low, int high, int i) {
-    if (low == high) {
-        return A[low];
-    }
-
-    // Find a good pivot using the median of medians
-    int x = medianOfMedians(A, low, high);
+int select(vector<int>& arr, int p, int q, int i) {
+    if (p == q)
+        return arr[p];
     
-    // Partition the array around the pivot
-    int x_idx = partition(A, low, high);
+    int x = median_of_medians(arr, p, q);
+    int pivot_idx = partition(arr, p, q, x);
+    int k = pivot_idx - p + 1;
 
-    int k = x_idx - low + 1;  // Number of elements in the left partition
+    if (i == k)
+        return arr[pivot_idx];
+    else if (i < k)
+        return select(arr, p, pivot_idx - 1, i);
+    else
+        return select(arr, pivot_idx + 1, q, i - k);
+}
 
-    if (i == k) {
-        return A[x_idx];
-    } else if (i < k) {
-        return select(A, low, x_idx - 1, i);
-    } else {
-        return select(A, x_idx + 1, high, i - k);
-    }
+int kth_smallest_element(vector<int>& arr, int k) {
+    return select(arr, 0, arr.size() - 1, k);
 }
 
 int main() {
-    // Seed for random number generation
-    srand(time(0));
-
-    // Generate the array A = {1, 2, 3, ..., 100}
-    vector<int> A(100);
+    vector<int> arr(100);
     for (int i = 0; i < 100; i++) {
-        A[i] = i + 1;
+        arr[i] = i + 1;
     }
 
-    // Create a random number generator
     random_device rd;
-    mt19937 g(rd());  // Mersenne Twister random number generator
+    mt19937 g(rd());
+    shuffle(arr.begin(), arr.end(), g);
 
-    // Shuffle the array using std::shuffle (C++11 and later)
-    shuffle(A.begin(), A.end(), g);
+    cout << "Shuffled input array A: ";
+    copy(arr.begin(), arr.end(), ostream_iterator<int>(cout, " "));
+    cout << endl;
 
-    // Test Select (Median of Medians) to find the 50th smallest element
-    int selectResult = select(A, 0, A.size() - 1, 50);
-    cout << "50th smallest element using Select (Median of Medians): " << selectResult << endl;
-
+    for (int k = 1; k <= 10; k++) {
+        int result = kth_smallest_element(arr, k);
+        if (result != -1) {
+            cout << "The " << k << "-th smallest element: " << result << endl;
+        }
+    }
     return 0;
 }
