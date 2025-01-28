625a31e25812a3fc3315d15a5bcd61c9b575f8d3
Indrajeet Aditya Roy <roy.indrajeet009@gmail.com>
2025-01-28 12:14:48 -0500

    implemented 1.2.2

--- EECE5643-Simulation-and-Performance-Evaluation/Ssq1.java
+++ EECE5643-Simulation-and-Performance-Evaluation/Ssq1.java
@@ -53,8 +53,13 @@ class Ssq1 {
       double service;                           /* service time         */
       double wait;                              /* delay + service      */
       double departure = START;                 /* departure time       */
+      double max_delay = 0.0;
+      double delayed_job_count = 0;
       Ssq1Sum sum = new Ssq1Sum();
       sum.initSumParas();
+
+      ArrayList<Double> arrival_times   = new ArrayList<Double>();
+      ArrayList<Double> departure_times = new ArrayList<Double>();
 
       while ( (line = in.readLine()) != null ) {
         index++;
@@ -64,22 +69,44 @@ class Ssq1 {
           delay    = departure - arrival;       /* delay in queue    */
         else
           delay    = 0.0;                       /* no delay          */
+        
+        delayed_job_count += (delay > 0.0) ? 1 : 0;
+
         service = Double.parseDouble(st.nextToken());
         wait         = delay + service;
         departure    = arrival + wait;          /* time of departure */
         sum.delay   += delay;
         sum.wait    += wait;
         sum.service += service;
+
+        max_delay = Math.max(max_delay, delay);
+
+        arrival_times.add(arrival);
+        departure_times.add(departure);
       }
       sum.interarrival = arrival - START;
 
       DecimalFormat f = new DecimalFormat("###0.00");
+
+      Collections.sort(arrival_times);
+      Collections.sort(departure_times);
+
+      int alphaT = countLE(arrival_times, 400);
+      int deltaT = countLE(departure_times, 400);
+      int num_jobs_at_t = alphaT - deltaT;
+
+      double delayed_jobs = delayed_job_count / index;
+      double utilization = sum.service / departure;
 
       System.out.println("\nfor " + index + " jobs");
       System.out.println("   average interarrival time =  " + f.format(sum.interarrival / index));
       System.out.println("   average service time .... =  " + f.format(sum.service / index));
       System.out.println("   average delay ........... =  " + f.format(sum.delay / index));
       System.out.println("   average wait ............ =  " + f.format(sum.wait / index));
+      System.out.println("   maximum delay ........... = " + max_delay);
+      System.out.println("   number of jobs in the serivce node at t=400 = " + num_jobs_at_t);
+      System.out.println("   proportion of jobs delayed = " + String.format("%.2f%%", delayed_jobs * 100));
+      System.out.println("   service utilization = " + String.format("%.2f%%", utilization * 100));
 
     } catch (EOFException eofe) {
       System.out.println("Ssq1:" + eofe);
@@ -87,5 +114,16 @@ class Ssq1 {
     // if the file opened okay, make sure we close it 
     fis.close();
   }
-    
+
+  public static int countLE(ArrayList<Double> list, double t) {
+    int idx = Collections.binarySearch(list, t);
+    if (idx < 0) {
+        idx = -idx - 1;
+    } else {
+        while ((idx + 1) < list.size() && list.get(idx+1) <= t) {
+            idx++;
+        }
+    }
+    return idx + 1;
+  }
 }
