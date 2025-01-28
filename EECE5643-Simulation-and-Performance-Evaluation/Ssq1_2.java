de8ede82edca3a56c2d1b2f1b956ff31e1a1e354
Indrajeet Aditya Roy <roy.indrajeet009@gmail.com>
2025-01-28 13:26:44 -0500

    implemented ex 1.2.6

--- EECE5643-Simulation-and-Performance-Evaluation/Ssq1_2.java
+++ EECE5643-Simulation-and-Performance-Evaluation/Ssq1_2.java
@@ -1,15 +1,3 @@
-
-/* -------------------------------------------------------------------------
- * This program simulates a single-server FIFO service node using arrival
- * times and service times read from a text file.  The server is assumed
- * to be idle when the first job arrives.  All jobs are processed completely
- * so that the server is again idle at the end of the simulation.   The
- * output statistics are the average interarrival time, average service
- * time, the average delay in the queue, and the average wait in the service 
- * node.
- * ------------------------------------------------------------------------- 
- */
-
 import java.io.*;
 import java.util.*;
 import java.text.*;
@@ -28,8 +16,8 @@ class Ssq1Sum {                                 /* sum of ...           */
   }
 }
 
-class Ssq1 {
-  static String FILENAME = "Ssq1.dat";          /* input data file */
+class Ssq1_2 {
+  static String FILENAME = "ac.dat";          /* input data file */
   static double START = 0.0;
 
   public static void main(String[] args) throws IOException {
@@ -53,13 +41,8 @@ class Ssq1 {
       double service;                           /* service time         */
       double wait;                              /* delay + service      */
       double departure = START;                 /* departure time       */
-      double max_delay = 0.0;
-      double delayed_job_count = 0;
       Ssq1Sum sum = new Ssq1Sum();
       sum.initSumParas();
-
-      ArrayList<Double> arrival_times   = new ArrayList<Double>();
-      ArrayList<Double> departure_times = new ArrayList<Double>();
 
       while ( (line = in.readLine()) != null ) {
         index++;
@@ -69,44 +52,32 @@ class Ssq1 {
           delay    = departure - arrival;       /* delay in queue    */
         else
           delay    = 0.0;                       /* no delay          */
+          
+        departure = Double.parseDouble(st.nextToken());
+        service= departure- arrival-delay;
         
-        delayed_job_count += (delay > 0.0) ? 1 : 0;
-
-        service = Double.parseDouble(st.nextToken());
         wait         = delay + service;
-        departure    = arrival + wait;          /* time of departure */
         sum.delay   += delay;
         sum.wait    += wait;
         sum.service += service;
-
-        max_delay = Math.max(max_delay, delay);
-
-        arrival_times.add(arrival);
-        departure_times.add(departure);
       }
       sum.interarrival = arrival - START;
 
       DecimalFormat f = new DecimalFormat("###0.00");
 
-      Collections.sort(arrival_times);
-      Collections.sort(departure_times);
-
-      int jobs_before_t = count_jobs(arrival_times, 400);
-      int jobs_after_t = count_jobs(departure_times, 400);
-      int num_jobs_at_t = jobs_before_t - jobs_after_t;
-
-      double delayed_jobs = delayed_job_count / index;
+      double avg_service_time = sum.service / index;
       double utilization = sum.service / departure;
+      double traffic_intensity = departure/arrival*utilization;
 
       System.out.println("\nfor " + index + " jobs");
       System.out.println("   average interarrival time =  " + f.format(sum.interarrival / index));
       System.out.println("   average service time .... =  " + f.format(sum.service / index));
       System.out.println("   average delay ........... =  " + f.format(sum.delay / index));
       System.out.println("   average wait ............ =  " + f.format(sum.wait / index));
-      System.out.println("   maximum delay ........... = " + f.format(max_delay));
-      System.out.println("   number of jobs in the serivce node at t=400 = " + num_jobs_at_t);
-      System.out.println("   proportion of jobs delayed = " + f.format(delayed_jobs));
-      System.out.println("   server utilization = " + f.format(utilization));
+
+      System.out.println("   average service time = " + f.format(avg_service_time));
+      System.out.println("   server utilization   = " + f.format(utilization));
+      System.out.println("   traffic intensity    = " + f.format(traffic_intensity));
 
     } catch (EOFException eofe) {
       System.out.println("Ssq1:" + eofe);
@@ -114,17 +85,5 @@ class Ssq1 {
     // if the file opened okay, make sure we close it 
     fis.close();
   }
-
-  public static int count_jobs(ArrayList<Double> list, double t) {
-    int idx = Collections.binarySearch(list, t);
-    if (idx < 0) {
-        idx = -idx - 1;
-    } else {
-        while ((idx + 1) < list.size() && list.get(idx+1) <= t) {
-            idx++;
-        }
-    }
-    return idx + 1;
-  }
-
+    
 }
